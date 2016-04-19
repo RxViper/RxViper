c5fda3a7b4f809f1ebf7a35689ed1b0dc440b2ef
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2016-04-19 11:05:08 +0300

    deprecate View in favor of ViewCallbacks

--- viper/src/main/java/viper/View.java
+++ viper/src/main/java/viper/View.java
@@ -1,10 +1,12 @@
 package viper;
 
 /**
- * ~ ~ ~ ~ Description ~ ~ ~ ~
+ * Will be removed in next release.<br>
+ * Use {@link ViewCallbacks} instead.
  *
  * @author Dmitriy Zaitsev
  * @since 2016-Feb-13, 22:35
  */
+@Deprecated
 public interface View {
 }
---
+++ viper/src/main/java/viper/ViewCallbacks.java
@@ -0,0 +1,10 @@
+package viper;
+
+/**
+ * ~ ~ ~ ~ Description ~ ~ ~ ~
+ *
+ * @author Dmitriy Zaitsev
+ * @since 2016-Apr-19, 11:00
+ */
+public interface ViewCallbacks extends View {
+}
