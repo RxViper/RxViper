96f345ff5ffb10195b14042c4f5df938f8f699a6
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2016-05-13 10:28:09 +0300

    Overload Interactor.execute() methods to allow using lambdas. Closes #3.

--- viper/src/main/java/viper/Interactor.java
+++ viper/src/main/java/viper/Interactor.java
@@ -4,6 +4,9 @@ import rx.Observable;
 import rx.Scheduler;
 import rx.Subscriber;
 import rx.Subscription;
+import rx.exceptions.OnErrorNotImplementedException;
+import rx.functions.Action0;
+import rx.functions.Action1;
 import rx.subscriptions.Subscriptions;
 
 /**
@@ -32,6 +35,75 @@ public abstract class Interactor<Param, Result> {
     execute(subscriber, null);
   }
 
+  public void execute(final Action1<? super Result> onNext) {
+    execute(onNext, (Param) null);
+  }
+
+  public void execute(final Action1<? super Result> onNext, Param param) {
+    checkNotNull(onNext, "onNext");
+
+    execute(new Subscriber<Result>() {
+      @Override public void onCompleted() {
+      }
+
+      @Override public void onError(final Throwable e) {
+        throw new OnErrorNotImplementedException(e);
+      }
+
+      @Override public void onNext(final Result result) {
+        onNext.call(result);
+      }
+    });
+  }
+
+  public void execute(final Action1<? super Result> onNext, final Action1<Throwable> onError) {
+    execute(onNext, onError, (Param) null);
+  }
+
+  public void execute(final Action1<? super Result> onNext, final Action1<Throwable> onError, Param param) {
+    checkNotNull(onNext, "onNext");
+    checkNotNull(onError, "onError");
+
+    execute(new Subscriber<Result>() {
+      @Override public void onCompleted() {
+      }
+
+      @Override public void onError(final Throwable e) {
+        onError.call(e);
+      }
+
+      @Override public void onNext(final Result result) {
+        onNext.call(result);
+      }
+    });
+  }
+
+  public void execute(final Action1<? super Result> onNext, final Action1<Throwable> onError,
+      final Action0 onComplete) {
+    execute(onNext, onError, onComplete, null);
+  }
+
+  public void execute(final Action1<? super Result> onNext, final Action1<Throwable> onError, final Action0 onComplete,
+      Param param) {
+    checkNotNull(onNext, "onNext");
+    checkNotNull(onError, "onError");
+    checkNotNull(onComplete, "onComplete");
+
+    execute(new Subscriber<Result>() {
+      @Override public void onCompleted() {
+        onComplete.call();
+      }
+
+      @Override public void onError(final Throwable e) {
+        onError.call(e);
+      }
+
+      @Override public void onNext(final Result result) {
+        onNext.call(result);
+      }
+    });
+  }
+
   public void unsubscribe() {
     if (mSubscription.isUnsubscribed()) {
       return;
@@ -41,4 +113,10 @@ public abstract class Interactor<Param, Result> {
   }
 
   protected abstract Observable<Result> createObservable(Param param);
+
+  private static <T> void checkNotNull(T arg, String name) {
+    if (arg == null) {
+      throw new IllegalArgumentException(name + " can not be null");
+    }
+  }
 }
