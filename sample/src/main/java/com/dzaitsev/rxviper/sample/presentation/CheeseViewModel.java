95815566189bbcfacde4e92ac3cbd10dd3d9340e
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2016-07-12 22:59:18 +0300

    change root package to `com.dzaitsev.rxviper`
    Resolves #39

---
+++ sample/src/main/java/com/dzaitsev/rxviper/sample/presentation/CheeseViewModel.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 Dmytro Zaitsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dzaitsev.rxviper.sample.presentation;
+
+/**
+ * ~ ~ ~ ~ Description ~ ~ ~ ~
+ *
+ * @author Dmytro Zaitsev
+ * @since 2016-Jun-07, 11:51
+ */
+public final class CheeseViewModel {
+  private final String  mName;
+  private final long    mId;
+  private       boolean isChecked;
+
+  public CheeseViewModel(String name, long id) {
+    mName = name;
+    mId = id;
+  }
+
+  public long getId() {
+    return mId;
+  }
+
+  public String getName() {
+    return mName;
+  }
+
+  public boolean isChecked() {
+    return isChecked;
+  }
+
+  public void setChecked(boolean checked) {
+    isChecked = checked;
+  }
+}
