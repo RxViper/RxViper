3fd5478a69a50a2d60f1fd51ba6a9f8e110e4fac
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2017-02-06 18:47:14 +0200

    add new features

--- buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/FeatureOptions.kt
+++ buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/FeatureOptions.kt
@@ -1,20 +1,65 @@
 package com.dzaitsev.rxviper.plugin
 
-data class FeatureOptions(private val _name: String) {
-  val name = _name.firstUpper()
-  var packageName = ""
-  var withTests = false
-  var hasRouter = true
-  var splitPackage = RxViperDSL.splitPackages
-  var useLambdas = RxViperDSL.useLambdas
-  var justMvp = RxViperDSL.justMvp
-  var skipExisting = RxViperDSL.skipExisting
-  var useCases = emptyArray<String>()
-  var routesTo = emptyArray<String>()
-  var requestModel = clazz<Any>()
-  var responseModel = clazz<Any>()
-
+class FeatureOptions(private val _name: String) {
+  val name = _name.capitalize()
   val fullPackage: String
     get() = "$packageName.${_name.toLowerCase()}"
+
+  internal var packageName = RxViperDSL.packageName
+  internal var hasRouter = RxViperDSL.hasRouter
+  internal var splitPackages = RxViperDSL.splitPackages
+  internal var useLambdas = RxViperDSL.useLambdas
+  internal var justMvp = RxViperDSL.justMvp
+  internal var skipExisting = RxViperDSL.skipExisting
+  internal var withTests = RxViperDSL.withTests
+
+  internal var useCases = emptyArray<String>()
+  internal var routesTo = emptyArray<String>()
+  internal var requestModel = clazz<Any>()
+  internal var responseModel = clazz<Any>()
+
+  fun packageName(value: String) {
+    packageName = value
+  }
+
+  fun splitPackages(value: Boolean) {
+    splitPackages = value
+  }
+
+  fun useLambdas(value: Boolean) {
+    useLambdas = value
+  }
+
+  fun justMvp(value: Boolean) {
+    justMvp = value
+  }
+
+  fun skipExisting(value: Boolean) {
+    skipExisting = value
+  }
+
+  fun hasRouter(value: Boolean) {
+    hasRouter = value
+  }
+
+  fun withTests(value: Boolean) {
+    withTests = value
+  }
+
+  fun useCases(value: Array<String>) {
+    useCases = value
+  }
+
+  fun routesTo(value: Array<String>) {
+    routesTo = value
+  }
+
+  fun requestModel(value: Class<Any>) {
+    requestModel = value
+  }
+
+  fun responseModel(value: Class<Any>) {
+    responseModel = value
+  }
 }
 
--- buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperPlugin.kt
+++ buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperPlugin.kt
@@ -2,56 +2,33 @@ package com.dzaitsev.rxviper.plugin
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import java.io.File
 
 class RxViperPlugin : Plugin<Project> {
 
-  override fun apply(project: Project) {
-    if (!(project.has<LibraryPlugin>() || project.has<AppPlugin>())) {
-      throw IllegalStateException("RxViper plugin can only be applied to android projects")
-    }
+  override fun apply(project: Project) = with(project) {
+    val isLibrary = has<LibraryPlugin>()
+    check(isLibrary || has<AppPlugin>()) { "RxViper plugin can only be applied to android projects" }
 
-    val rxViper = project.extensions.create("rxViper", clazz<RxViperDSL>(), project.container(clazz<FeatureOptions>()))
-    val generateScreensTask = project.task<GeneratingTask>("generateScreens")
+    val rxViper = extensions.create("rxViper", clazz<RxViperDSL>(), container(clazz<FeatureOptions>()))
 
-    generateScreensTask.doLast {
-      val android = project.getExtension<AppExtension>("android")
+    afterEvaluate {
+      with(task<GeneratingTask>("generateScreens")) {
+        configure(this) { group = "RxViper" }
 
-      val mainSourceSets = android.sourceSets.toSet().filter { "main" == it.name }
-      if (mainSourceSets.isNotEmpty()) {
-        val mainSourceSet = mainSourceSets[0]
-        val srcDirs = mainSourceSet.java.srcDirs
-
-        if (!srcDirs.isEmpty()) {
-          val srcMainJava = srcDirs.toList()[0]
-          rxViper.featureOptionsContainer.all {
-            createRxViper(it, srcMainJava)
-          }
+        options = rxViper.options
+        directory = if (isLibrary) {
+          getExtension<LibraryExtension>("android")
+        } else {
+          getExtension<AppExtension>("android")
         }
+            .sourceSets.findByName("main")
+            .java
+            .srcDirs.first()
       }
-    }
-  }
-
-  private fun createRxViper(feature: FeatureOptions, directory: File) {
-    val list = mutableListOf(ViewCallbacksGenerator(feature),
-        PresenterGenerator(feature, feature.hasRouter))
-    if (feature.hasRouter) {
-      list.add(RouterGenerator(feature))
-    }
-    if (!feature.justMvp) {
-      list.add(InteractorGenerator(feature))
-    }
-
-    list.forEach { generator ->
-      //            val path = "src/main/java/${feature.fullPackage.replace('.', File.separatorChar)}"
-//            val directory = File(path)
-//            if (!directory.exists()) {
-//                directory.mkdirs()
-//            }
-      generator.saveTo(directory)
     }
   }
 }
