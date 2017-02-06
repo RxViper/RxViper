460e2ac54d7acda2696822421f3c1531c4e2f7be
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2017-02-06 18:47:14 +0200

    add new features

--- buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperDSL.kt
+++ buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperDSL.kt
@@ -3,15 +3,47 @@ package com.dzaitsev.rxviper.plugin
 import org.gradle.api.Action
 import org.gradle.api.NamedDomainObjectContainer
 
-open class RxViperDSL(val featureOptionsContainer: NamedDomainObjectContainer<FeatureOptions>) {
+open class RxViperDSL(val options: NamedDomainObjectContainer<FeatureOptions>) {
+
   fun screens(action: Action<NamedDomainObjectContainer<FeatureOptions>>) {
-    action.execute(featureOptionsContainer)
+    action.execute(options)
+  }
+
+  fun packageName(value: String) {
+    packageName = value
+  }
+
+  fun splitPackages(value: Boolean) {
+    splitPackages = value
+  }
+
+  fun useLambdas(value: Boolean) {
+    useLambdas = value
+  }
+
+  fun justMvp(value: Boolean) {
+    justMvp = value
+  }
+
+  fun skipExisting(value: Boolean) {
+    skipExisting = value
+  }
+
+  fun hasRouter(value: Boolean) {
+    hasRouter = value
+  }
+
+  fun withTests(value: Boolean) {
+    withTests = value
   }
 
   companion object {
-    @JvmStatic var splitPackages = false
-    @JvmStatic var useLambdas = false
-    @JvmStatic var justMvp = false
-    @JvmStatic var skipExisting = false
+    internal var packageName = "generated"
+    internal var splitPackages = false
+    internal var useLambdas = false
+    internal var justMvp = false
+    internal var skipExisting = false
+    internal var hasRouter = true
+    internal var withTests = false
   }
 }
