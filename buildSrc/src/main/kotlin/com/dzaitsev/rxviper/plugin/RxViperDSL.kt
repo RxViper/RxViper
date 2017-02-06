934d585c274ef06b502a9b72b0a4bb991f39411c
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2017-02-06 22:56:32 +0200

    improve dsl

--- buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperDSL.kt
+++ buildSrc/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperDSL.kt
@@ -3,47 +3,32 @@ package com.dzaitsev.rxviper.plugin
 import org.gradle.api.Action
 import org.gradle.api.NamedDomainObjectContainer
 
-open class RxViperDSL(val options: NamedDomainObjectContainer<FeatureOptions>) {
+open class RxViperDSL(internal val options: NamedDomainObjectContainer<FeatureOptions>) {
 
   fun screens(action: Action<NamedDomainObjectContainer<FeatureOptions>>) {
     action.execute(options)
   }
 
-  fun packageName(value: String) {
-    packageName = value
-  }
-
-  fun splitPackages(value: Boolean) {
-    splitPackages = value
-  }
-
-  fun useLambdas(value: Boolean) {
-    useLambdas = value
-  }
-
-  fun justMvp(value: Boolean) {
-    justMvp = value
-  }
-
-  fun skipExisting(value: Boolean) {
-    skipExisting = value
-  }
-
-  fun hasRouter(value: Boolean) {
-    hasRouter = value
-  }
-
-  fun withTests(value: Boolean) {
-    withTests = value
-  }
-
   companion object {
-    internal var packageName = "generated"
-    internal var splitPackages = false
-    internal var useLambdas = false
-    internal var justMvp = false
-    internal var skipExisting = false
-    internal var hasRouter = true
-    internal var withTests = false
+    @JvmStatic var packageName = "generated"
+      @JvmName("packageName") set
+
+    @JvmStatic var splitPackages = false
+      @JvmName("splitPackages") set
+
+    @JvmStatic var useLambdas = false
+      @JvmName("useLambdas") set
+
+    @JvmStatic var justMvp = false
+      @JvmName("justMvp") set
+
+    @JvmStatic var skipExisting = false
+      @JvmName("skipExisting") set
+
+    @JvmStatic var hasRouter = true
+      @JvmName("hasRouter") set
+
+    @JvmStatic var withTests = false
+      @JvmName("withTests") set
   }
 }
