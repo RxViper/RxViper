f26fe263c61291b0a591ca9de29eb03e133f410f
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2017-02-15 15:14:59 +0200

    Plugin: rename requestModel -> requestClass; internal changes

--- rxviper-gradle-plugin/src/main/kotlin/com/dzaitsev/rxviper/plugin/internal/codegen/InteractorGenerator.kt
+++ rxviper-gradle-plugin/src/main/kotlin/com/dzaitsev/rxviper/plugin/internal/codegen/InteractorGenerator.kt
@@ -16,24 +16,28 @@ internal class InteractorGenerator(screen: Screen) : Generator(screen) {
   override fun createSpec(): List<TypeSpec.Builder> = if (screen.useCases.isEmpty()) {
     listOf(create(typeSpecName, aClass(), aClass()))
   } else {
-    screen.useCases.map { create("${it.name}Interactor", it.requestModel, it.responseModel) }
+    screen.useCases.map { create("${it.name}Interactor", it.requestClass, it.responseClass) }
   }
 
-  private fun create(name: String, requestModel: Class<Any>, responseModel: Class<Any>) = TypeSpec.classBuilder(name)
-      .superclass(ParameterizedTypeName.get(aClass<Interactor<*, *>>(), requestModel, responseModel))
-      .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-      .addMethod(MethodSpec.constructorBuilder()
-          .addModifiers(Modifier.PUBLIC)
-          .addParameter(aClass<Scheduler>(), "subscribeScheduler")
-          .addParameter(aClass<Scheduler>(), "observeScheduler")
-          .addStatement("super(subscribeScheduler, observeScheduler)")
-          .build())
-      .addMethod(MethodSpec.methodBuilder("createObservable")
-          .addModifiers(Modifier.PROTECTED)
-          .addAnnotation(aClass<Override>())
-          .addParameter(requestModel, "requestModel")
-          .addComment("TODO: Write your business logic here...")
-          .addStatement("return \$T.empty()", aClass<Observable<*>>())
-          .returns(ParameterizedTypeName.get(aClass<Observable<*>>(), responseModel))
-          .build())
+  private fun create(name: String, requestClass: Class<Any>, responseClass: Class<Any>): TypeSpec.Builder {
+    val subscribeScheduler = "subscribeScheduler"
+    val observeScheduler = "observeScheduler"
+    return TypeSpec.classBuilder(name)
+        .superclass(ParameterizedTypeName.get(aClass<Interactor<*, *>>(), requestClass, responseClass))
+        .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+        .addMethod(MethodSpec.constructorBuilder()
+            .addModifiers(Modifier.PUBLIC)
+            .addParameter(aClass<Scheduler>(), subscribeScheduler)
+            .addParameter(aClass<Scheduler>(), observeScheduler)
+            .addStatement("super(\$N, \$N)", subscribeScheduler, observeScheduler)
+            .build())
+        .addMethod(MethodSpec.methodBuilder("createObservable")
+            .addModifiers(Modifier.PROTECTED)
+            .addAnnotation(aClass<Override>())
+            .addParameter(requestClass, "requestModel")
+            .addComment("TODO: Write your business logic here...")
+            .addStatement("return \$T.empty()", aClass<Observable<*>>())
+            .returns(ParameterizedTypeName.get(aClass<Observable<*>>(), responseClass))
+            .build())
+  }
 }
