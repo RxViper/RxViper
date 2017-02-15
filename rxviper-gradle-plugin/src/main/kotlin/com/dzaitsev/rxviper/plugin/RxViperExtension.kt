5d22c5b8fdc793ee997027f2b88c49bc03a955e9
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2017-02-15 22:47:24 +0200

    Plugin: reconfigure project

--- rxviper-gradle-plugin/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperExtension.kt
+++ rxviper-gradle-plugin/src/main/kotlin/com/dzaitsev/rxviper/plugin/RxViperExtension.kt
@@ -2,7 +2,6 @@ package com.dzaitsev.rxviper.plugin
 
 import com.dzaitsev.rxviper.plugin.internal.dsl.Screen
 import groovy.lang.Closure
-import org.gradle.api.Incubating
 import org.gradle.api.NamedDomainObjectContainer
 
 open class RxViperExtension(val screens: NamedDomainObjectContainer<Screen>) {
@@ -23,14 +22,5 @@ open class RxViperExtension(val screens: NamedDomainObjectContainer<Screen>) {
     @JvmStatic var hasRouter = true; @JvmName("hasRouter") set
 
     @JvmStatic var addMetaInfo = true; @JvmName("addMetaInfo") set
-
-    @Incubating
-    @JvmStatic internal var skipExisting = false; @JvmName("skipExisting") set
-
-    @Incubating
-    @JvmStatic internal var splitPackages = false; @JvmName("splitPackages") set
-
-    @Incubating
-    @JvmStatic internal var withTests = false; @JvmName("withTests") set
   }
 }
