0d62d0992fb76883ac42fb969e109f42ca87a60a
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2018-02-15 00:40:12 +0200

    implement AlternateExecutor

--- viper-executors/src/main/java/com/dzaitsev/viper/executors/AlternateExecutor.java
+++ viper-executors/src/main/java/com/dzaitsev/viper/executors/AlternateExecutor.java
@@ -16,9 +16,8 @@
 
 package com.dzaitsev.viper.executors;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.dzaitsev.viper.Intrinsics.requireNotNull;
@@ -30,21 +29,48 @@ import static com.dzaitsev.viper.Intrinsics.requireNotNull;
  * @since 2017-Aug-15, 13:26
  */
 class AlternateExecutor implements Executor {
-  private final BlockingQueue<Runnable> queue   = new ArrayBlockingQueue<>(4);
-  private final AtomicInteger           counter = new AtomicInteger();
+  private final PriorityBlockingQueue<TimedDecorator> queue   = new PriorityBlockingQueue<>();
+  private final AtomicInteger                         counter = new AtomicInteger();
+  private final AtomicInteger                         wip     = new AtomicInteger();
 
   @Override
   public void execute(Runnable runnable) {
     requireNotNull(runnable, "runnable");
-    counter.incrementAndGet();
-    queue.add(runnable);
-    if (counter.getAndIncrement() == 0) {
+
+    final TimedDecorator timed = new TimedDecorator(runnable, System.currentTimeMillis(), counter.incrementAndGet());
+    queue.add(timed);
+
+    if (wip.getAndIncrement() == 0) {
       do {
-        final Runnable head = queue.poll();
+        final TimedDecorator head = queue.poll();
         if (head != null) {
           head.run();
         }
-      } while (counter.decrementAndGet() > 0);
+      } while (wip.decrementAndGet() > 0);
+    }
+  }
+
+  private static final class TimedDecorator implements Comparable<TimedDecorator>, Runnable {
+    private final Runnable delegate;
+    private final long     execTime;
+    private final int      count;
+
+    TimedDecorator(Runnable delegate, long execTime, int count) {
+      requireNotNull(delegate, "delegate");
+      this.delegate = delegate;
+      this.execTime = execTime;
+      this.count = count;
+    }
+
+    @Override
+    public int compareTo(TimedDecorator that) {
+      final int result = Long.compare(execTime, that.execTime);
+      return result == 0 ? Integer.compare(count, that.count) : result;
+    }
+
+    @Override
+    public void run() {
+      delegate.run();
     }
   }
 }
